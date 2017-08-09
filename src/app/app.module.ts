f495dcc509a1196302e5832713c85dda55cf07c1
bergenhem <cbergenhem@gmail.com>
2017-08-09 15:09:50 -0400

    Move bootstrap html to separate component for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BootstrapComponent } from './bootstrap.component';
 import { WidgetComponent } from './widget.component';
 import { AboutComponent } from './about.component';
 
 const appRoutes: Routes = [
+  { path: '', component: BootstrapComponent },
   { path: 'widgets', component: WidgetComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: BootstrapComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    BootstrapComponent,
     WidgetComponent,
     AboutComponent
   ],
